fac564d68c9f80affa81deda392c8375c42114c0
Rafael Thiago Pereira de Melo <rafael.pereira20@icloud.com>
2021-02-12 17:36:47 -0300

    Correção do cors

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
 import 'reflect-metadata';
-import express from 'express';
 import cors from 'cors';
+import express from 'express';
 import routes from './routes/index';
 
 import './database';
 
 const app = express();
+
 app.use(cors());
 
 app.use(express.json());
+
 app.use(routes);
 
 app.listen(3330, () => {
